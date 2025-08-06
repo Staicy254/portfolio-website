11646d47a7ccbcc5b2375b0d22ac619b5d6a5e0a
Jean Staicy Auma Otambo <jeanstaicy21@gmail.com>
2025-08-06 14:59:35 +0300

    otambo-library

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Otambo Library',
+  description: 'Done by Jean Otambo',
+  generator: 'Otambo.library',
 }
 
 export default function RootLayout({
